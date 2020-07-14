81dbf6b65e318d4388ffc3a4e7e9e88e5a856af2
ChristianVlad <Christian.valladarezz@gmail.com>
2020-07-14 00:05:59 -0600

    Adding a new java class "Usuario" and other things

--- spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
@@ -4,34 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.bolsaideas.springboot.form.app.domain.Usuario;
+
 
 @Controller
 public class FormController {
 	
 	@GetMapping("/form")
-	//El objeto model es para pasarlo a la vista.
 	public String form(Model model) {
 		model.addAttribute("titulo", "Formulario de usuario");
 		return "form";
 	}
 	
+	
+	
 	@PostMapping("/form")
-	public String procesar(Model model,
-			/*Con este @RequestParam recibe los datos
-			 Porque los extrae los campos que hicimos 
-			 en el file: form.html*/
-			@RequestParam String username,
-			@RequestParam String password,
-			@RequestParam String email) {
-		
-		
-		//El primero es el objeto/atributo, el segundo es el valor.
+	public String procesar(Usuario usuario, Model model) {
+
 		model.addAttribute("titulo", "Resultado form");
-		model.addAttribute("username", username);
-		model.addAttribute("password", password);
-		model.addAttribute("email", email);
-		
+		model.addAttribute("usuario", usuario);
+
 		return "resultado";
 	}
 }
